8529bde16c0ad46552679544aba97bc4a9febb15
Ayan De <deayan252@gmail.com>
2025-06-19 03:16:39 +0530

    Initial commit

--- infra/api.ts
+++ infra/api.ts
@@ -1,7 +1,9 @@
+import { handler } from "../packages/functions/src/api";
 import { bucket } from "./storage";
 
-export const myApi = new sst.aws.Function("MyApi", {
-  url: true,
-  link: [bucket],
-  handler: "packages/functions/src/api.handler"
-});
+export const myApi = new sst.aws.ApiGatewayV2("Api");
+
+myApi.route("GET /", {
+  link:[bucket],
+  handler
+})
