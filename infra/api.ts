03550ac3b574c49101a020a81ba4b3e65d123dea
Ayan De <deayan252@gmail.com>
2025-06-19 15:31:00 +0530

    docs: added some commentline

--- infra/api.ts
+++ infra/api.ts
@@ -5,6 +5,8 @@ export const myApi = new sst.aws.ApiGatewayV2("Api");
 //Here we are creating a simple API with one route, GET /. 
 // When this API is invoked, the function called
 //handler in packages/functions/src/api.ts will be executed.
+
+//We are also linking an S3 Bucket to our API. This allows the functions in our API to access the bucket.
 myApi.route("GET /", {
   link:[bucket],
   handler: "packages/functions/src/api.handler"
