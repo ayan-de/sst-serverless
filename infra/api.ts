aa12ba2ef4aed5e13f55b68c35e36005ee291afd
Ayan De <deayan252@gmail.com>
2025-06-19 12:43:13 +0530

    fix: bug in the handler of the api

--- infra/api.ts
+++ infra/api.ts
@@ -1,9 +1,11 @@
-import { handler } from "../packages/functions/src/api";
 import { bucket } from "./storage";
 
 export const myApi = new sst.aws.ApiGatewayV2("Api");
 
+//Here we are creating a simple API with one route, GET /. 
+// When this API is invoked, the function called
+//handler in packages/functions/src/api.ts will be executed.
 myApi.route("GET /", {
   link:[bucket],
-  handler
+  handler: "packages/functions/src/api.handler"
 })
