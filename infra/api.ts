c8fa45f30f05a83c53b99888e1d88f9c26d078cd
Ayan De <deayan252@gmail.com>
2025-06-26 17:10:10 +0530

    Enabling cors

--- infra/api.ts
+++ infra/api.ts
@@ -3,6 +3,10 @@ import { table } from "./storage";
 
 //We are creating an API using SST’s Api component. It creates an Amazon API Gateway HTTP API.
 export const api = new sst.aws.ApiGatewayV2("Api", {
+  cors: {
+    // We are allowing all methods to access our API.
+    allowMethods: ["GET"],
+  },
   //By using the transform prop we are telling the API that we want the given props to be applied to all the routes in our API.
   transform: {
     route: {
--- infra/storage.ts
+++ infra/storage.ts
@@ -1,5 +1,10 @@
 // Create an S3 bucket
-export const bucket = new sst.aws.Bucket("Uploads");
+export const bucket = new sst.aws.Bucket("Uploads",{
+    cors: {
+        // We are allowing all methods to access our API.
+        allowMethods: ["GET"]
+    }
+});
 
 // Create the DynamoDB table
 export const table = new sst.aws.Dynamo("Notes", {
