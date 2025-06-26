8209dc9580ab2378c025e390d6879d9813d7e944
Ayan De <deayan252@gmail.com>
2025-06-27 02:50:21 +0530

    feat: added frontend of our app - Added Tailwind CSS and Bootstrap dependencies to the project. - Removed unused Vite logo SVG file. - Refactored App component to include a Bootstrap Navbar and routing. - Created Routes component for handling application routes. - Implemented Home and NotFound components with basic styling. - Updated main entry point to use React Router for navigation.

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Refhired</h1>
+        <p className="text-muted">
+          Simplifying receiving and giving job referrals for absolutely everyone
+        </p>
+      </div>
+    </div>
+  );
+}
--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </StrictMode>
 );
