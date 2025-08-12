390b600fde0cd29d635327b405ca9ab53a6e34b1
Ayan De <deayan252@gmail.com>
2025-08-12 23:23:17 +0530

    reactor: name

--- packages/frontend/src/containers/Home.tsx
+++ packages/frontend/src/containers/Home.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="Home">
       <div className="lander">
-        <h1>Refhired</h1>
+        <h1>NotesApp</h1>
         <p className="text-muted">
           Simplifying receiving and giving job referrals for absolutely everyone
         </p>
