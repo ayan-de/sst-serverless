657b9082c7a96d731b56c24405a4c28cfe1320aa
Ayan De <deayan252@gmail.com>
2025-08-16 20:15:06 +0530

    feat: added a create note page

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from './containers/Home.tsx';
 import NotFound from './containers/NotFound.tsx';
 import Login from './containers/Login.tsx';
 import Signup from './containers/Signup.tsx';
+import NewNote from './containers/NewNote.tsx';
 export default function Links() {
   return (
     <Routes>
@@ -10,6 +11,7 @@ export default function Links() {
       <Route path="*" element={<NotFound />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/notes/new" element={<NewNote />} />
     </Routes>
   );
 }
